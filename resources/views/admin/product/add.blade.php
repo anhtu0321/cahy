23495291301e8b80e794041be4bc59c30162af11
Hoang Van Tu <vantu9486@gmail.com>
2021-01-18 16:26:05 +0700

    ckeditor

--- resources/views/admin/product/add.blade.php
+++ resources/views/admin/product/add.blade.php
@@ -3,7 +3,8 @@
     <title>Thêm Sản phẩm</title>
 @endsection
 @section('css')
-    <title>Thêm Sản phẩm</title>
+    <link href="{{ asset('vendors/select2/select2.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/admins/product/add/add.css') }}" rel="stylesheet" />
 @endsection
 @section('content')
     <div class="content-wrapper">
@@ -23,22 +24,30 @@
                             </div>
                             <div class="form-group">
                                 <label>Ảnh đại diện</label>
-                                <input class="form-control" type="file" name="feature_image_path">
+                                <input class="form-control-file" type="file" name="feature_image_path">
                             </div>
                             <div class="form-group">
                                 <label>Ảnh chi tiết</label>
-                                <input class="form-control" type="file" multiple name="image_path[]">
+                                <input class="form-control-file" type="file" multiple name="image_path[]">
                             </div>
                             <div class="form-group">
-                                <label>Chọn Danh mục cha</label>
-                                <select class="form-control" name="parent_id">
-                                    <option value="0">Chọn danh mục cha</option>
+                                <label>Chọn Danh mục</label>
+                                <select class="form-control select2_init" name="parent_id">
+                                    <option value="0">Chọn danh mục</option>
                                     {!! $option !!}
                                 </select>
                             </div>
                             <div class="form-group">
+                                <label>Nhâp tags cho sản phẩm</label>
+                                <select class="form-control tags_select_choose" multiple="multiple" name="tags[]">
+                                    
+                                </select>
+                            </div>
+                            <div class="form-group">
                                 <label>Nhập nội dung</label>
-                                <textarea class="form-control" name="content" rows="3"></textarea>
+                                <textarea class="form-control" name="content" id="content" rows="3"></textarea>
+                                
+                                
                             </div>
                             <button class="btn btn-primary" type="submit">Thêm</button>
                             @csrf
@@ -50,5 +59,23 @@
     </div>
 @endsection
 @section('js')
-    <title>Thêm Sản phẩm</title>
+    <script src="{{ asset('vendors/select2/select2.min.js') }}"></script>
+    <script src="{{ asset('/admins/product/add/add.js') }}"></script>
+    <script>
+        $(function(){
+            $(".tags_select_choose").select2({
+                tags:true,
+                tokenSeparators: [',',' ']
+            })
+            $(".select2_init").select2({
+                placeholder: "Chọn Danh mục",
+                allowClear:true
+            })
+        })
+    </script>
+    <script src="{{ asset('vendors/ckeditor/ckeditor.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('vendors/ckfinder/ckfinder.js')}}" type="text/javascript"></script>
+    <script>
+        CKEDITOR.replace("content");
+    </script>
 @endsection
